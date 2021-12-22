27ccf9564ad8234c37195ce1441515c78d989c7d
kamaal111 <kamaal.f1@gmail.com>
2021-12-22 11:23:48 +0100

    Initial commit

---
+++ Shared/FancyAlertPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyAlertPlayApp.swift
+//  Shared
+//
+//  Created by Kamaal M Farah on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct FancyAlertPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
